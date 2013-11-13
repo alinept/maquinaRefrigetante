a1f969c7937b40979d218d81ddd39a4c39dbe3ae
Aline Teixeira <aline.teixeira@ceuma.br>
2013-11-13 20:53:55 -0300

    commitando

--- src/ufma/engenharia/maquina/test/dao/VendaDAOTest.java
+++ src/ufma/engenharia/maquina/test/dao/VendaDAOTest.java
@@ -1,8 +1,15 @@
 package ufma.engenharia.maquina.test.dao;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import ufma.engenharia.maquina.dao.VendaDAO;
+import ufma.engenharia.maquina.dominio.Refrigerante;
+import ufma.engenharia.maquina.dominio.Venda;
 
 public class VendaDAOTest {
 	
@@ -11,12 +18,25 @@ public class VendaDAOTest {
 	@Test
 	public void retornaVendaRefri()
 	{
+		List<Venda> vendasRefri = new ArrayList<Venda>();
+		Refrigerante r = new Refrigerante();
+		r.setCodigo(1);
 		
+		vendasRefri = vendaDAO.recuperaVendas(r);
+		
+		assertNotNull(vendasRefri);
 	}
 	
 	@Test
 	public void retornaTodasVendas()
 	{
+		List<Venda> vendas = new ArrayList<Venda>();
+		Refrigerante r = new Refrigerante();
+		r.setCodigo(1);
+		
+		vendas = vendaDAO.recuperarTodasVendas();
+		
+		assertNotNull(vendas);
 		
 	}
 	
